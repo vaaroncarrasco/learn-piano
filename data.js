bd86e4fbfa22885fd2b39f939719c6a78cad2385
Aaron Carrasco <vaaroncarrasco@gmail.com>
2022-04-03 00:30:11 -0500

    moved obj from script.js to data.js

---
+++ data.js
@@ -0,0 +1,123 @@
+const allChords = {
+  cMajor: {
+    title: 'C',
+    notes: 'G-C-E',
+    src: './chords-audio/C.m4a'
+  },
+  cMinor: {
+    title: 'Cm',
+    notes: 'G-C-Eb',
+    src: './chords-audio/Cm.m4a'
+  },
+  cSharp: {
+    title: 'C#/Db',
+    notes: 'G#-C#-F',
+    src: './chords-audio/C#-Db.m4a'
+  },
+  cSharpMinor: {
+    title: 'C#m/Dbm',
+    notes: 'G#-D#-E',
+    src: './chords-audio/C#m-Dbm.m4a'
+  },
+  dMajor: {
+    title: 'D',
+    notes: 'A-D-F#',
+    src: './chords-audio/D.m4a'
+  },
+  dMinor: {
+    title: 'Dm',
+    notes: 'A-D-F',
+    src: './chords-audio/Dm.m4a'
+  },
+  dSharp: {
+    title: 'D#/Eb',
+    notes: 'G-A#-D#',
+    src: './chords-audio/D#-Eb.m4a'
+  },
+  dSharpMinor: {
+    title: 'D#m/Ebm',
+    notes: 'F#-A#-D#',
+    src: './chords-audio/D#-Ebm.m4a'
+  },
+  eMajor: {
+    title: 'E',
+    notes: 'G#-B-D',
+    src: './chords-audio/E.m4a'
+  },
+  eMinor: {
+    title: 'Em',
+    notes: 'G-B-D',
+    src: './chords-audio/Em.m4a'
+  },
+  fMajor: {
+    title: 'F',
+    notes: 'A-C-F',
+    src: './chords-audio/F.m4a'
+  },
+  fMinor: {
+    title: 'Fm',
+    notes: 'Ab-C-F',
+    src: './chords-audio/Fm.m4a'
+  },
+  fSharp: {
+    title: 'F#/Gb',
+    notes: 'A#-C#-F#',
+    src: './chords-audio/F#-Gb.m4a'
+  },
+  fSharpMinor: {
+    title: 'F#m/Gbm',
+    notes: 'A-C#-F#',
+    src: './chords-audio/F#m-Gbm.m4a'
+  },
+  gMajor: {
+    title: 'G',
+    notes: 'B-D-G',
+    src: './chords-audio/G.m4a'
+  },
+  gMinor: {
+    title: 'Gm',
+    notes: 'Bb-D-G',
+    src: './chords-audio/Gm.m4a'
+  },
+  gSharp: {
+    title: 'G#/Ab',
+    notes: 'C-D#-G#',
+    src: './chords-audio/G#-Ab.m4a'
+  },
+  gSharpMinor: {
+    title: 'G#m/Abm',
+    notes: 'B-D#-G#',
+    src: './chords-audio/G#m-Abm.m4a'
+  },
+  aMajor: {
+    title: 'A',
+    notes: 'E-A-C#',
+    src: './chords-audio/A.m4a'
+  },
+  aMinor: {
+    title: 'Am',
+    notes: 'E-A-C',
+    src: './chords-audio/Am.m4a'
+  },
+  aSharp: {
+    title: 'A#/Bb',
+    notes: 'F-A#-D',
+    src: './chords-audio/A#-Bb.m4a'
+  },
+  aSharpMinor: {
+    title: 'A#m/Bbm',
+    notes: 'F-A#-Db',
+    src: './chords-audio/A#m-Bbm.m4a'
+  },
+  bMajor: {
+    title: 'B',
+    notes: 'F#-B-D#',
+    src: './chords-audio/B.m4a'
+  },
+  bMinor: {
+    title: 'Am',
+    notes: 'F#-B-D',
+    src: './chords-audio/Bm.m4a'
+  },
+
+}
