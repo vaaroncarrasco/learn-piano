8ff3e7724f822d839b6c500b277882424805744d
Aaron Carrasco <vaaroncarrasco@gmail.com>
2022-04-11 03:43:45 -0500

    script logic works

---
+++ script.js
@@ -0,0 +1,57 @@
+
+const footer = document.getElementById('footer');
+const keySelect = document.getElementById('key-select');
+const keyContainer = document.getElementById('key-container');
+
+const playChord = (chord) => {
+  console.log(chord.id)
+  console.log(chord)
+  const chordElement = document.getElementById(chord.id);
+  chordElement.play();
+}
+
+Object.entries(chords).map(chord => {
+  footer.insertAdjacentHTML('afterend', `
+    <audio id="${chord[1].title}" src="${chord[1].src}"></audio>
+  `);
+});
+
+Object.entries(keys).map(key => {
+  keySelect.insertAdjacentHTML('beforeend', `
+    <option value="${key[0]}">${key[0]}</option>
+  `)
+})
+
+const renderButtons = ({ chords }) => {
+
+  keyContainer.innerHTML = '';
+  let htmlChords = '';
+
+   chords.forEach(chord => {
+
+    htmlChords += `
+      <div onclick="playChord(${chord.title})">
+        <h1>${chord.title.replace('_','/')}</h1>
+        <p>${chord.notes}</p>
+      </div>
+    `
+  });
+
+  keyContainer.insertAdjacentHTML('beforeend', htmlChords);
+
+}
+
+keySelect.addEventListener('change', (e) => {
+
+  let selectedKey = e.target.value;
+
+  Object.entries(keys).filter(key => {
+
+    if (key[0] == selectedKey) {
+      const keyChords = { chords: key[1] }
+      renderButtons(keyChords)
+    }
+
+  })
+
+})
